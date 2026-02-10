e17c7384d1671ccb9cc6f27b1b6a71375b8ba68c
Luiz1405 <kubaszewski001@hotmail.com>
2026-02-10 20:28:39 -0300

    feat(database): database module
    Adiciona database module especifico para o postgres
    
    Refs: Task 2

--- apps/love-travel-back/src/app.module.ts
+++ apps/love-travel-back/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/env.validation';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -8,7 +9,8 @@ import { envValidationSchema } from './config/env.validation';
       isGlobal: true,
       validationSchema: envValidationSchema,
       envFilePath: '.env',
-    })
+    }),
+    DatabaseModule
   ],
 
 })
