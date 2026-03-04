87b96e9d4a3af02346cf650bf56c88c305003714
Luiz1405 <kubaszewski001@hotmail.com>
2026-03-04 11:32:52 -0300

    feature(user): controller user
    Adiciona controllers com o crud completo de usuarios.
    
    Refs: Task 5

--- apps/love-travel-back/src/modules/users/controllers/user.controller.ts
+++ apps/love-travel-back/src/modules/users/controllers/user.controller.ts
@@ -1,18 +1,45 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/updat-user.dto';
+import { UserEntity } from '../entities/user.entity';
 
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
+    private excludePassword(user: UserEntity): Omit<UserEntity, 'password'> {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+
+    @Get()
+    async findAll() {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string) {
+        return this.usersService.findById(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        const user = await this.usersService.update(id, updateUserDto);
+        return this.excludePassword(user);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return this.usersService.delete(id);
+    }
+
     @Post()
     async create(@Body() createUserDto: CreateUserDto) {
         const user = await this.usersService.create(createUserDto);
-
-        delete user.password;
-        return user;
+        return this.excludePassword(user);
     }
 
 }
